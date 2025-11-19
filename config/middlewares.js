62863b99f3f6eb880c5eeace8f50952f02e3e0b2
ethyashpathak <ethyashpathak@gmail.com>
2025-11-19 20:24:56 +0530

    strapi-backend init

---
+++ config/middlewares.js
@@ -0,0 +1,36 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", "http://localhost:5173", "https://your-frontend-domain.com"],
+          'img-src': ["'self'", "data:", "blob:", "http://localhost:1337", "https://your-frontend-domain.com"],
+          'media-src': ["'self'", "data:", "blob:", "http://localhost:1337"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173',
+        'https://your-frontend-domain.com',
+      ],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
